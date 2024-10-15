ff95b8614586f6940e6b328b958bbd1dc90814f4
KAREN GISEL LOPEZ <karenglopez1020@gmail.com>
2024-10-14 19:26:17 -0500

    Listo el sign-up y login según el rol

--- src/components/login.js
+++ src/components/login.js
@@ -7,21 +7,19 @@ document.addEventListener("DOMContentLoaded", function () {
   const submitLink = document.querySelector(".btn2 a");
   const toggleButton = document.getElementById("toggle-btn");
   const extraOptions = document.getElementById("extra-options");
-  const admin=document.getElementById("btn0")
   const socialLogin = document.querySelector(".social-login");
   const email = document.getElementById("email-field");
   const confirm_password = document.getElementById("confirm-password-field");
+  const rolfield = document.getElementById("rol-field");
   let isLogin = true;
 
   function showError(inputElement, message) {
-    // Eliminar cualquier mensaje de error existente
     const existingError =
       inputElement.parentElement.querySelector(".error-message");
     if (existingError) {
       existingError.remove();
     }
 
-    // Crear e insertar el nuevo mensaje
     const errorDiv = document.createElement("div");
     errorDiv.className = "error-message";
     errorDiv.textContent = message;
@@ -36,8 +34,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 3000);
   }
 
-  async function loginUser(email, password) {
-    const url = "http://localhost:8001/user/validate"; // URL del endpoint de tu backend
+  async function loginUser(username, password) {
+    const url = "http://localhost:8001/user/validate";
 
     try {
       const response = await fetch(url, {
@@ -46,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          email: email,
+          email: username,
           password: password,
         }),
       });
@@ -65,7 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  async function registerUser(username, email, password, confirm_password) {
+  async function registerUser(user, adminCode = null) {
     const url = "http://localhost:8001/save-user/";
 
     try {
@@ -75,12 +73,14 @@ document.addEventListener("DOMContentLoaded", function () {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name: username,
-          email: email,
-          password: password,
-          confirm_password: confirm_password,
+          ...user,
+          admin_code: adminCode, // Enviar el código de administrador solo si es proporcionado
         }),
       });
+
+      if (!response.ok) {
+        throw new Error("Error en el registro de usuario");
+      }
 
       const data = await response.json();
       console.log("Registro exitoso:", data);
@@ -91,7 +91,6 @@ document.addEventListener("DOMContentLoaded", function () {
       alert(
         "Error en el registro. Verifique su conexión o intente nuevamente."
       );
-      return null;
     }
   }
 
@@ -101,53 +100,53 @@ document.addEventListener("DOMContentLoaded", function () {
     const password = document.getElementById("password");
     const emailInput = document.getElementById("email");
     const confirmPasswordInput = document.getElementById("confirm_password");
-    const adminCodeField = document.getElementById("input");
+    const adminCodeInput = document.getElementById("admin-code");
+    const rolSelect = document.getElementById("rol-list");
 
-  // Validar campo de usuario
-  if (!username.value.trim()) {
-    showError(username, "Ingrese el usuario.");
-    isValid = false;
-  }
-
-  // Validar campo de contraseña
-  if (!password.value.trim()) {
-    showError(password, "Ingrese la contraseña.");
-    isValid = false;
-  }
-
-   // Validar campo de código de administrador solo si está visible
-  if (adminCodeField && window.getComputedStyle(adminCodeField).display !== "none" && !adminCodeField.value.trim()) {
-    showError(adminCodeField, "Ingrese el código de administrador.");
-    isValid = false;
-  }
-
-  // Si no es modo login, validar campos adicionales
-  if (!isLogin) {
-    
-    if (!emailInput.value.trim()) {
-      showError(emailInput, "Ingrese un correo electrónico.");
+    if (!username.value.trim()) {
+      showError(username, "Ingrese el usuario.");
       isValid = false;
     }
 
-    // Validar confirmación de contraseña
-    if (!confirmPasswordInput.value.trim()) {
-      showError(confirmPasswordInput, "Confirme su contraseña.");
-      isValid = false;
-    } else if (password.value !== confirmPasswordInput.value) {
-      showError(confirmPasswordInput, "Las contraseñas no coinciden.");
+    if (!password.value.trim()) {
+      showError(password, "Ingrese la contraseña.");
       isValid = false;
     }
-  }
 
-  return isValid;
-}
+    if (!isLogin) {
+      if (!emailInput.value.trim()) {
+        showError(emailInput, "Ingrese un correo electrónico.");
+        isValid = false;
+      }
+
+      if (!confirmPasswordInput.value.trim()) {
+        showError(confirmPasswordInput, "Confirme su contraseña.");
+        isValid = false;
+      } else if (password.value !== confirmPasswordInput.value) {
+        showError(confirmPasswordInput, "Las contraseñas no coinciden.");
+        isValid = false;
+      }
+
+      // Solo validar el código de administrador si el rol seleccionado es "admin"
+      if (rolSelect.value === "admin") {
+        if (
+          adminCodeInput &&
+          window.getComputedStyle(adminCodeInput).display !== "none" &&
+          !adminCodeInput.value.trim()
+        ) {
+          showError(adminCodeInput, "Ingrese el código de administrador.");
+          isValid = false;
+        }
+      }
+    }
+
+    return isValid;
+  }
 
   function switchToLogin() {
     formContainer.classList.toggle("slide-left");
     formContainer.classList.toggle("slide-right");
     form.reset();
-
-    // Eliminar cualquier mensaje de error existente
     document.querySelectorAll(".error-message").forEach((el) => el.remove());
 
     imageContainer2.querySelector("img").classList.toggle("zoom-in");
@@ -157,11 +156,11 @@ document.addEventListener("DOMContentLoaded", function () {
       formTitle.innerText = "Login";
       email.style.display = "none";
       confirm_password.style.display = "none";
+      rolfield.style.display = "none";
       extraOptions.style.display = "flex";
-      admin.style.display="flex";
       socialLogin.style.display = "block";
       submitLink.innerText = "Login";
-      toggleButton.innerHTML = ' <i class="fas fa-arrow-left"></i>Sign Up';
+      toggleButton.innerHTML = '<i class="fas fa-arrow-left"></i>Sign Up';
       isLogin = true;
     }, 500);
   }
@@ -179,8 +178,8 @@ document.addEventListener("DOMContentLoaded", function () {
       formTitle.innerText = "Sign Up";
       email.style.display = "block";
       confirm_password.style.display = "block";
+      rolfield.style.display = "block";
       extraOptions.style.display = "none";
-      admin.style.display="none";
       socialLogin.style.display = "block";
       submitLink.innerText = "Sign Up";
       toggleButton.innerHTML = ' Login<i class="fas fa-arrow-right"></i>';
@@ -188,50 +187,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 500);
   }
 
-  submitLink.addEventListener("click", async function (e) {
-    e.preventDefault();
-
-    if (validateForm()) {
-      if (isLogin) {
-        // Si es login, obtenemos el email y la contraseña y llamamos a loginUser
-        const emailValue = document.getElementById("username").value;
-        const passwordValue = document.getElementById("password").value;
-
-        // Llamar a la función de login
-        const loginResponse = await loginUser(emailValue, passwordValue);
-
-        if (loginResponse) {
-          // Si el login es exitoso, redirigimos al chat
-          window.location.href = "../public/chat.html";
-        } else {
-          alert("Login fallido. Verifica tus credenciales.");
-        }
-      } else {
-        // Registro de usuario
-        const usernameValue = document.getElementById("username").value;
-        const emailValue = document.getElementById("email").value;
-        const passwordValue = document.getElementById("password").value;
-        const confirmPasswordValue =
-          document.getElementById("confirm_password").value;
-
-        // Llamar a la función de registro
-        const registerResponse = await registerUser(
-          usernameValue,
-          emailValue,
-          passwordValue,
-          confirmPasswordValue
-        );
-
-        if (registerResponse) {
-          alert("Usuario registrado exitosamente. Bienvenido");
-          window.location.href = "../public/chat.html";
-        } else {
-          alert("Registro fallido. Inténtalo de nuevo.");
-        }
-      }
-    }
-  });
-
+  // Cambiar entre Login y Sign Up
   toggleButton.addEventListener("click", function () {
     if (isLogin) {
       switchToSignUp();
@@ -240,211 +196,69 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  const btn1 = document.getElementById("toggle-btn");
-  let movedRight = false;
+  // Detectar cambios en el rol seleccionado
+  const rolField = document.getElementById("rol-list");
+  const adminCodeField = document.getElementById("admin-code-field");
 
-  btn1.addEventListener("click", function () {
-    if (movedRight) {
-      btn1.classList.remove("move-left");
-      btn1.classList.add("move-right");
+  rolField.addEventListener("change", function () {
+    if (rolField.value === "admin") {
+      adminCodeField.style.display = "block";
     } else {
-      btn1.classList.remove("move-right");
-      btn1.classList.add("move-left");
+      adminCodeField.style.display = "none";
     }
-    movedRight = !movedRight;
   });
- 
-  // Función para cambiar directamente a Sign Up si hay un hash en la URL
-  function directToSignUp() {
-    // Cambiar a Sign Up sin animación
-    formTitle.innerText = "Sign Up";
-    email.style.display = "block";
-    confirm_password.style.display = "block";
-    extraOptions.style.display = "none";
-    admin.style.display="none";
-    adminCodeField.style.display = "none";
-    socialLogin.style.display = "block";
-    submitLink.innerText = "Sign Up";
-    toggleButton.innerHTML = '<i class="fas fa-arrow-left"></i> Login';
-    isLogin = false;
 
-    const btn1 = document.getElementById("toggle-btn");
-    let movedRight = false;
+  // Enviar formulario
+  submitLink.addEventListener("click", async function (e) {
+    e.preventDefault();
 
-    btn1.addEventListener("click", function () {
-      if (movedRight) {
-        btn1.classList.remove("move-left");
-        btn1.classList.add("move-right");
+    if (validateForm()) {
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const email = document.getElementById("email").value;
+      const confirmPassword = document.getElementById("confirm_password").value;
+      const rol = document.getElementById("rol-list").value;
+      const adminCode = document.getElementById("admin-code").value;
+
+      if (isLogin) {
+        // Lógica de login
+        const loginResponse = await loginUser(username, password);
+
+        if (loginResponse) {
+          console.log("Rol del usuario:", loginResponse.rol); // Añade esta línea
+          if (
+            loginResponse.rol === "Administrador" ||
+            loginResponse.rol === "admin"
+          ) {
+            window.location.href = "../public/admin-dashboard.html"; // Redirigir al dashboard de administrador
+          } else {
+            window.location.href = "../public/chat.html"; // Redirigir a la página de usuario
+          }
+        }
       } else {
-        btn1.classList.remove("move-right");
-        btn1.classList.add("move-left");
-      }
-      movedRight = !movedRight;
-    });
-  }
+        // Registro de usuario (Sign-Up)
+        const user = {
+          name: username,
+          email: email,
+          password: password,
+          confirm_password: confirmPassword,
+          rol: rol,
+        };
 
-  // Si la URL tiene el hash #signup, cambiamos a Sign Up inmediatamente
-  if (window.location.hash === "#signup") {
-    directToSignUp();
-  }
-});
-
-//administrador
-document.addEventListener("DOMContentLoaded", function () {
-  const adminButton = document.getElementById("btn0");
-  const formTitle = document.getElementById("form-title");
-  const username = document.getElementById("username");
-  const password = document.getElementById("password");
-  const adminCodeField = document.getElementById("input");
-  const forgotPasswordLink = document.querySelector("#extra-options a");
-  const adminButtonContainer = document.getElementById("btn0");
-  const cambiarSinLogin = document.getElementById("toggle-btn")
-  const socialLogin = document.querySelector(".social-login");
-
-    // Función para cambiar el formulario al login de administrador
-    function switchToAdminLogin() {
-      formTitle.innerText = "Login Administrador";
-      formTitle.style.fontSize = "50px";
-      formTitle.style.marginLeft = "-20px"
-      adminCodeField.style.display = "block"; 
-
-      forgotPasswordLink.style.display = "none";  
-      adminButtonContainer.style.display = "none"; 
-      socialLogin.style.display = "none"; 
-
-    }
-  
-    adminButton.addEventListener("click", function (e) {
-      e.preventDefault();
-      switchToAdminLogin(); 
-    });
-
-    function switchToSignUp() {
-      adminCodeField.style.display = "none"; 
-  
-    }
-  
-    cambiarSinLogin.addEventListener("click", function (e) {
-      e.preventDefault();
-      switchToSignUp();
-    });
-
-    //parte de validar
-    document.querySelector(".btn2 a").addEventListener("click", async function (e) {
-      e.preventDefault();
-  
-      const adminCode = document.getElementById("input").value;
-      
-      if (adminCode) {
-        const registerResponse = await registerAdmin(
-          username.value,
-          password.value,
-          adminCode
-        );
-        if (registerResponse) {
-          alert("Administrador registrado con éxito");
-          window.location.href = "../public/admin-dashboard.html"; 
+        if (rol === "admin") {
+          const registerResponse = await registerUser(user, adminCode);
+          if (registerResponse) {
+            alert("Administrador registrado exitosamente.");
+            window.location.href = "../public/chat.html"; // Redirigir después del registro
+          }
         } else {
-          alert("Error en el registro de administrador.");
+          const registerResponse = await registerUser(user);
+          if (registerResponse) {
+            alert("Usuario registrado exitosamente.");
+            window.location.href = "../public/chat.html";
+          }
         }
-      }
-    });
-  
-    // Función para el registro del administrador(si se va a realizar de manera normal)
-    async function registerAdmin(username, password, adminCode) {
-      const url = "http://localhost:8001/register-admin"; 
-      try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: username,
-            password: password,
-            adminCode: adminCode,
-          }),
-        });
-        if (!response.ok) {
-          throw new Error("Error en el registro de administrador");
-        }
-        return await response.json();
-      } catch (error) {
-        console.error("Error:", error.message);
-        return null;
       }
     }
   });
-
-
-//la otra forma como actualizar usuario
-document.querySelector(".btn2 a").addEventListener("click", async function (e) {
-  e.preventDefault();
-
-  const adminCode = document.getElementById("input").value;
-  
-  if (adminCode) {
-    // validar usuario
-    const loginResponse = await validateUser(username.value, password.value);
-    if (loginResponse) {
-      
-      const updateResponse = await updateUserRole(loginResponse.id, "admin");
-      if (updateResponse) {
-        alert("Rol actualizado a administrador con éxito");
-        window.location.href = "../public/chat.html"; 
-      } else {
-        alert("Error al actualizar el rol a administrador.");
-      }
-    } else {
-      alert("Error en la autenticación del usuario.");
-    }
-  }
 });
-
-async function validateUser(email, password) {
-  const url = "http://localhost:8001/user/validate";
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error("Error en la validación del usuario");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error.message);
-    return null;
-  }
-}
-
-async function updateUserRole(userId, role) {
-  const url = `http://localhost:8001/update-rol/${userId}`;
-  try {
-    const response = await fetch(url, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        role: role,
-      }),
-    });
-    if (!response.ok) {
-      throw new Error("Error al actualizar el rol del usuario");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error:", error.message);
-    return null;
-  }
-}
-
-
-  
